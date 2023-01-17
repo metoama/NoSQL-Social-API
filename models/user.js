9320e35564fc5ef592465f102322428577be8a9a
CAGE1899 <metoama@gmail.com>
2023-01-17 01:16:39 -0500

    initial commit for models file structure

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema ({
+    usernmae: {
+        type: String, 
+        required: true,
+        unique: true,
+        trim: true 
+    },
+    email: {
+        type: String, 
+        required: true,
+        unique: true,
+        match: //check mongoose documentation
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+});
+
+module.exports = mongoose.model('User', UserSchema);
