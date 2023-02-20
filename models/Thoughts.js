63d22874b0fceb8c512e8ec5532b9dec66cf6fb7
CAGE1899 <metoama@gmail.com>
2023-02-20 03:17:47 -0500

    updating the Thoughts model

---
+++ models/Thoughts.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true, 
+            minLength: 1, 
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formatDate,
+        },
+        username: {
+                type: String, 
+                required: true,
+            },
+        },
+        {
+            reactions: [reactionSchema],
+        },
+        {
+            toJSON: {
+                virtuals: true, 
+                getters: true,
+                
+            },
+            id: false,
+        }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
